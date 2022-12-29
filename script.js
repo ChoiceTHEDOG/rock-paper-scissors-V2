93d1396626e2505fcbc561186324035609e28992
valentin <valen.manestar@gmail.com>
2022-12-29 20:50:17 -0300

    listos los botones, funcionales a nivel basico

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let gamesPlayed = 0
 
 // Here we get the computer choice using a random number generator 
 
-function getComputerChoice() {
+function ComputerChoice() {
     if(numForSelection >= 1 && numForSelection <= 33) {
         return "rock"
     } else if (numForSelection >= 34 && numForSelection <= 66) {    
@@ -17,55 +17,81 @@ function getComputerChoice() {
 
 // Ask the user for his choice 
 
-function getPlayerChoice(selection) {
-    selection = prompt("Whats your choice?")
-    return selection.toLowerCase();
-}
+// function playerChoice(selection) {
+//     return selection;
+// }
 
 // Single round logic
 
-function playSingleRound(playerSelection, computerSelectio) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();    
+function playRound(playerSelection, computerSelection = ComputerChoice()) {
     if(playerSelection == computerSelection) {
-        return "Its a Tie"
+        console.log( "Its a Tie")
     } else if(playerSelection == "rock" && computerSelection == "scissors") {
         playerScore = playerScore + 1;
-        return "You Win!!! Rock beats Scissors"
+        console.log( "You Win!!! Rock beats Scissors")
     } else if(playerSelection == "rock" && computerSelection == "paper") {
         computerScore =+ 1;
-        return "The computer Wins :(. Paper beats Rock"
+        console.log( "The computer Wins :(. Paper beats Rock")
     } else if(playerSelection == "paper" && computerSelection == "rock") {
         playerScore =+ 1;
-        return "You Win!!! Paper beats Rock"
+        console.log( "You Win!!! Paper beats Rock")
     } else if(playerSelection == "paper" && computerSelection == "scissors") {
         computerScore =+ 1
-        return "The computer Wins :(. Scissors beats paper"
+        console.log( "The computer Wins :(. Scissors beats paper")
     } else if(playerSelection == "scissors" && computerSelection == "paper") {
         playerScore =+ 1;
-        return "You Win!!! Scissors beats Paper"
+        console.log( "You Win!!! Scissors beats Paper")
     } else if(playerSelection == "scissors" && computerSelection == "rock") {
         computerScore =+ 1;
-        return "The computer Wins :(. Scissors beats paper"
+        console.log( "The computer Wins :(. Scissors beats paper")
     } else {
-        return "invalid selection"
+        console.log( "invalid selection")
     }
 
 }
 
 numForSelection= Math.floor(Math.random() * 100) + 1;
 
-function game(){
-    do {
-        playSingleRound();
-        gamesPlayed = gamesPlayed + 1;
-    } while (gamesPlayed < 5);
-    if(playerScore > computerScore){
-        return "The Player wins whit " + playerScore + " points!!!";
-       } else if (playerScore < computerScore) {
-        return "The Computer wins whit " + computerScore + " points!!!";
-       } else {
-        return "Its a Tie"
-       }
-}
-console.log(game())
+// function game(){
+//     do {
+//         playRound();
+//         gamesPlayed = gamesPlayed + 1;
+//     } while (gamesPlayed < 5);
+//     if(playerScore > computerScore){
+//         return "The Player wins whit " + playerScore + " points!!!";
+//        } else if (playerScore < computerScore) {
+//         return "The Computer wins whit " + computerScore + " points!!!";
+//        } else {
+//         return "Its a Tie"
+//        }
+// }
+// console.log(game())
+
+// UI design
+
+const mainContainer = document.querySelector("div");
+
+// buttons creation
+
+const rock = document.createElement("button");
+rock.textContent = "ROCK";
+rock.addEventListener("click", () => {playRound("rock")});
+
+const paper = document.createElement("button");
+paper.textContent = "PAPER";
+paper.addEventListener("click", () => {playRound("paper")});
+
+const scissors = document.createElement("button");
+scissors.textContent = "SCISSORS";
+scissors.addEventListener("click", () => {playRound("scissors")});
+
+mainContainer.appendChild(rock);
+mainContainer.appendChild(paper);
+mainContainer.appendChild(scissors);
+
+// function test(){
+//     alert("aaaaaaaa")
+// }
+
+// const buttons = document.querySelectorAll("button"); 
+// buttons.forEach(button => button.addEventListener("click", test))

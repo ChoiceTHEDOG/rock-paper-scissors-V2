c12df7f24edf0715cba22348375a13e69b79742a
valentin <valen.manestar@gmail.com>
2023-01-06 19:31:02 -0300

    proyecto estilizado, (mas feo q la mierda)

--- script.js
+++ script.js
@@ -10,37 +10,49 @@ const mainContainer = document.querySelector("div");
 
 // buttons creation
 
+const buttonContainer = document.createElement("div");
+buttonContainer.setAttribute("id", "buttonContainer")
+mainContainer.appendChild(buttonContainer);
+
 const rock = document.createElement("button");
 rock.textContent = "ROCK";
-rock.classList.add("button")
+rock.classList.add("buttons")
 rock.addEventListener("click", () => {playRound("rock")});
-mainContainer.appendChild(rock);
+buttonContainer.appendChild(rock);
 
 const paper = document.createElement("button");
 paper.textContent = "PAPER";
-paper.classList.add("button")
+paper.classList.add("buttons")
 paper.addEventListener("click", () => {playRound("paper")});
-mainContainer.appendChild(paper);
+buttonContainer.appendChild(paper);
 
 const scissors = document.createElement("button");
 scissors.textContent = "SCISSORS";
-scissors.classList.add("button")
+scissors.classList.add("buttons")
 scissors.addEventListener("click", () => {playRound("scissors")});
-mainContainer.appendChild(scissors);
+buttonContainer.appendChild(scissors);
 
 // results display 
 
 const displayDiv = document.createElement("div");
 displayDiv.setAttribute("id", "displayDiv");
-mainContainer.insertBefore(displayDiv, rock);
+mainContainer.insertBefore(displayDiv, buttonContainer);
 
 const playerResults = document.createElement("div");
 playerResults.setAttribute("id", "playerResultDiv");
+playerResults.classList.add("viewDivs");
 displayDiv.appendChild(playerResults);
 
 const computerResults = document.createElement("div");
 computerResults.setAttribute("id", "computerResultDiv");
+computerResults.classList.add("viewDivs");
 displayDiv.appendChild(computerResults);
+
+const winnerDisplay = document.createElement("div");
+winnerDisplay.setAttribute("id", "winnerDisplay")
+winnerDisplay.classList.add("viewDivs");
+winnerDisplay.textContent = "Click a button to play!"
+displayDiv.insertBefore(winnerDisplay, computerResults);
 
 // Here we get the computer choice using a random number generator 
 
@@ -72,37 +84,37 @@ updatePlayerResults();
 function playRound(playerSelection, computerSelection = ComputerChoice()) {
     numForSelection = Math.floor(Math.random() * 100) + 1;
     if(playerSelection == computerSelection) {
-        alert("Its a Tie");
+        winnerDisplay.textContent = "Its a Tie";
     } else if(playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
         updatePlayerResults();
         winner();
-        return "You Win!!! Rock beats Scissors"
+        winnerDisplay.textContent = "You Win!!! Rock beats Scissors"
     } else if(playerSelection == "rock" && computerSelection == "paper") {
         computerScore++;
         updateComputerResults();
         winner();
-        return "The computer Wins :( Paper beats Rock"
+        winnerDisplay.textContent = "The computer Wins :( Paper beats Rock"
     } else if(playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
         updatePlayerResults();
         winner();
-        return "You Win!!! Paper beats Rock"
+        winnerDisplay.textContent = "You Win!!! Paper beats Rock"
     } else if(playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++;
         updateComputerResults();
         winner();
-        return "The computer Wins :(. Scissors beats paper"
+        winnerDisplay.textContent = "The computer Wins :( Scissors beats paper"
     } else if(playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
         updatePlayerResults();
         winner();
-        return "You Win!!! Scissors beats Paper"
+        winnerDisplay.textContent = "You Win!!! Scissors beats Paper"
     } else if(playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++;
         updateComputerResults();
         winner();
-        return "The computer Wins :( Rock beats paper"
+        winnerDisplay.textContent = "The computer Wins :( Rock beats paper"
     } 
 }
 
@@ -110,8 +122,8 @@ function playRound(playerSelection, computerSelection = ComputerChoice()) {
 
 function winner(){
     if(playerScore == 5) {
-        alert("The player wins!!!") 
+        alert("The player wins the game!!!") 
     } else if(computerScore == 5) {
-        alert("The computer wins!!!")
+        alert("The computer wins! the game!!!")
     } 
 }
